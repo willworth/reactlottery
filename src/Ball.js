4c2a23ec78e1df20054f0e9d7b2ebef1cae9bfbf
Will Worth <will.worth@gmail.com>
2019-06-07 13:24:52 +0200

    ball component

---
+++ src/Ball.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Ball.css";
+
+export default class Ball extends Component {
+  render() {
+    return <div className="Ball">{this.props.num}</div>;
+  }
+}
